91045219675e99bfa30f51f7397a2d555cd9dce3
James Chen <dumganhar@gmail.com>
2022-08-12 10:31:59 +0800

    Add cocos license and disable clang-format for generated code

--- Source/Modules/cocos.cxx
+++ Source/Modules/cocos.cxx
@@ -271,6 +271,35 @@ static void cocos_emit_return_variable(Node *n, SwigType *rt, Wrapper *f) {
   }
 }
 
+static void appendCocosLicense(File *f, uint32_t year) {
+    Printf(f, R"(
+/****************************************************************************
+ Copyright (c) %u Xiamen Yaji Software Co., Ltd.
+
+ http://www.cocos.com
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated engine source code (the "Software"), a limited,
+ worldwide, royalty-free, non-assignable, revocable and non-exclusive license
+ to use Cocos Creator solely to develop games on your target platforms. You shall
+ not use Cocos Creator software for developing other software or tools that's
+ used for developing games. You are not granted to publish, distribute,
+ sublicense, and/or sell copies of Cocos Creator.
+
+ The software or tools in this License Agreement are licensed, not sold.
+ Xiamen Yaji Software Co., Ltd. reserves all rights not expressly granted to you.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+****************************************************************************/
+)", year);
+}
+
 /**
  * A convenience class to manage state variables for emitters.
  * The implementation delegates to SWIG Hash DOHs and provides
@@ -2110,6 +2139,9 @@ int CocosEmitter::initialize(Node *n) {
         Exit(EXIT_FAILURE);
     }
 
+    Printv(f_wrap_cpp, "// clang-format off\n\n", 0);
+    Printv(f_wrap_h, "// clang-format off\n\n", 0);
+
     /* Initialization of members */
     s_runtime = NewString("");
     s_init = NewString("");
@@ -2126,6 +2158,9 @@ int CocosEmitter::initialize(Node *n) {
 
     Swig_banner(f_wrap_cpp);
     Swig_banner(f_wrap_h);
+
+    appendCocosLicense(f_wrap_cpp, 2022);
+    appendCocosLicense(f_wrap_h, 2022);
 
     return SWIG_OK;
 }
@@ -2164,6 +2199,9 @@ int CocosEmitter::dump(Node *n) {
     }
 
     Printv(f_wrap_h, state.globals(HEADER_REGISTER_CLASSES), 0);
+
+    Printv(f_wrap_cpp, "// clang-format on\n", 0);
+    Printv(f_wrap_h, "// clang-format on\n", 0);
 
     return SWIG_OK;
 }
